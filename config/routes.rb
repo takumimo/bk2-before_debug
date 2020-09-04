90dd7bef5e62225f93ddd13f75034ef88a9e3dc9
motokitakumi <mtktkm242424@gmail.com>
2020-09-04 17:34:26 +0900

    [Finish]

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: 'home#top'
+  get 'home/about' => 'home#about'
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
+  
+  
+end
